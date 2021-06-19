3451fdd9cc20103c15ca63cade9255437cd3964d
arpitnath <arpitnath42@gmail.com>
2021-06-20 05:22:11 +0530

    npm error fix & spinner fixed with an assumtion

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
-import MainPage from "./containers/MainPage";
-import { reducers } from "./reducers";
-import { rootSaga } from "./sagas";
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import createSagaMiddleware from 'redux-saga'
+import MainPage from './containers/MainPage'
+import { reducers } from './reducers'
+import { rootSaga } from './sagas'
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware()
 
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+)
 
-sagaMiddleware.run(rootSaga, store.dispatch);
+sagaMiddleware.run(rootSaga, store.dispatch)
 
 function App() {
   return (
     <Provider store={store}>
       <MainPage />
     </Provider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/SpinnerLogo/index.tsx
+++ src/components/SpinnerLogo/index.tsx
@@ -1,13 +1,25 @@
-import * as React from "react";
-import { SpinnerLogoWrapper, SpinnerLogoImage } from "./styles";
-import { SpinnerLogoProps } from "./interfaces";
+import * as React from 'react'
+import { SpinnerLogoWrapper, SpinnerLogoImage } from './styles'
+import { SpinnerLogoProps } from './interfaces'
+import { useSelector } from 'react-redux'
+import { AppState } from '../../reducers'
 
 const SpinnerLogo: React.FC<SpinnerLogoProps> = ({ isSpinning, logo }) => {
+  const { isRotating } = useSelector<AppState, AppState['animationsState']>(
+    (state) => state.animationsState
+  )
   return (
     <SpinnerLogoWrapper>
-      <SpinnerLogoImage isSpinning={isSpinning} src={logo} />
+      <SpinnerLogoImage isSpinning={isRotating} src={logo} />
     </SpinnerLogoWrapper>
-  );
-};
+  )
+}
 
-export default SpinnerLogo;
+export default SpinnerLogo
+
+/**
+ * @Assumtion
+ * if we are already passing the
+ * {isSpinning} prop as true or false then the
+ * spinner either rotates or stays still
+ */
